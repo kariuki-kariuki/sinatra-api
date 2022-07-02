3e29490f0e42b581039423f2118da4ca6edaee0e
Kariuki <mkmartinoes@gmail.com>
2022-07-02 15:23:27 +0300

    fx: Seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_02_105146) do
+ActiveRecord::Schema.define(version: 2022_06_28_180326) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.float "balance"
+    t.integer "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "businesses", force: :cascade do |t|
     t.string "item_name"
@@ -20,6 +27,33 @@ ActiveRecord::Schema.define(version: 2022_07_02_105146) do
     t.string "business_name"
     t.string "image"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.integer "sender"
+    t.integer "receiver"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "text_massage"
+    t.integer "sender"
+    t.integer "receiver"
+    t.string "typ"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.integer "phone"
+    t.string "image"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,25 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+User.all.delete_all
 User.create(username: "Emkay", phone: 742075647, password: "wueh", email: "emkay@gmail.com", image: "'https://d346xxcyottdqx.cloudfront.net/wp-content/uploads/2016/04/Digital-696x371.jpg")
 User.create(username: "Sammy Doe", phone: 742075648, password: "wueh", email: "node@gmail.com", image: "https://d346xxcyottdqx.cloudfront.net/wp-content/uploads/2016/04/Digital-696x371.jpg")
 User.create(username: "Wendy", phone: 742, password: "wueh", email: "emkay@gmail.com", image: "https://d346xxcyottdqx.cloudfront.net/wp-content/uploads/2016/04/Digital-696x371.jpg")
 User.create(username: "John", phone: 743, password: "wueh", email: "emkay@gmail.com", image: "https://d346xxcyottdqx.cloudfront.net/wp-content/uploads/2016/04/Digital-696x371.jpg")
 
+Message.all.delete_all
 Message.create(text_massage: "Hello Emkay", sender: 742, receiver: 742075647, typ: "text")
 Message.create(text_massage: "Hello Wendy How are you doing?", sender: 742075647, receiver: 742, typ: "text")
 Message.create(text_massage: "I am fine thank I really miss you,  how are you?", sender: 742, receiver: 742075647, typ: "text")
 Message.create(text_massage: "I am doing good I miss you too.", sender: 742075647, receiver: 742, typ: "text")
 
+Account.all.delete_all
 Account.create(phone: 742075647, balance: 35600)
 Account.create(phone: 742, balance: 15600)
 Account.create(phone: 743, balance: 10600)
 Account.create(phone: 742075648, balance: 2900)
 
+Contact.all.delete_all
 Contact.create(sender: 742, receiver: 742075647, name: "Emkay")
 Contact.create(sender: 742, receiver: 743, name: "John")
 Contact.create(sender: 742, receiver: 742075648,  name:"Sam")

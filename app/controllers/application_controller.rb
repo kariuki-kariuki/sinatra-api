295ed4f54c3608a738e1ec21559049c36a914d32
Kariuki <mkmartinoes@gmail.com>
2022-07-05 17:53:00 +0300

    syntax correction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
+# class ApplicationController
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-
 
   def get_messages(attr)
     Message.where(["sender = '%s' and receiver = '%s'", attr[:currentuser], attr[:activechat]]).or(
@@ -9,25 +9,24 @@ class ApplicationController < Sinatra::Base
   end
 
   def create_bot(attr)
-    new_message = Message.create(text_massage: "Welcome You can ask this bot anything", sender: 7426, receiver: attr[:phone])
+    new_message = Message.create(text_massage: 'Welcome You can ask this bot anything', sender: 7426, receiver: attr[:phone])
     new_message.save
   end
-  
+
   def create_contact(attr)
-    contact_one = Contact.create(sender: attr[:phone], receiver: 7426, name: "Chatty Bot")
+    contact_one = Contact.create(sender: attr[:phone], receiver: 7426, name: 'Chatty Bot')
     contact_two = Contact.create(sender: 7426, receiver: attr[:phone], name: attr[:name])
     contact_one.save
     contact_two.save
-
   end
-  
+
   # Add your routes here
   def login(attr)
     user = User.where(["phone = '%s' and password = '%s'", attr[:phone], attr[:password]]).first
     if user != nil
-      {username: user[:username], email: user[:email], phone: user[:phone]}
+      { username: user[:username], email: user[:email], phone: user[:phone] }
     else
-      {data: "Error login"}
+      { data: 'Error login'}
     end
   end
 
@@ -42,54 +41,46 @@ class ApplicationController < Sinatra::Base
 
   def get_contacts(attr)
     contacts = Contact.where("sender = '%s'", attr[:phone])
-
     new_contact_details = contacts.map do |contact|
-      {name: contact.name, phone: contact.receiver, last_message: self.get_messages(currentuser: contact.sender, activechat: contact.receiver).last}
+      { name: contact.name, phone: contact.receiver, last_message: self.get_messages(currentuser: contact.sender, activechat: contact.receiver).last}
     end
     new_contact_details.to_json
   end
 
-  get "/contacts/:phone" do
-
+  get '/contacts/:phone' do
     self.get_contacts(phone: params[:phone])
-
   end
 
   def self.add_contact(attr)
     if !self.check_if_user_exist?(attr)
       new_contact = User.create(attr)
       new_contact.save
-
     else
       {}
     end
-
   end
 
-
-  #login
-  get "/login/:phone/:password" do
+  # login
+  get '/login/:phone/:password' do
     phones = self.login(phone: params[:phone], password: params[:password])
     phones.to_json
   end
 
-  post "/users" do 
-      user = User.create(
-        email: params[:email],
-        image: params[:image],
-        phone: params[:phone],
-        password: params[:password],
-        username: params[:username]
-      )
-      user.save
-
-      self.create_bot(phone: params[:phone])
-      self.create_contact(name: params[:username], phone: params[:phone])
-
+  post '/users' do
+    user = User.create(
+      email: params[:email],
+      image: params[:image],
+      phone: params[:phone],
+      password: params[:password],
+      username: params[:username]
+    )
+    user.save
+    self.create_bot(phone: params[:phone])
+    self.create_contact(name: params[:username], phone: params[:phone])
     { username: user[:username], email: user[:email], phone: user[:phone] }.to_json
   end
 
-  post "/messages/new" do 
+  post 'messages/new' do 
     new_message = Message.create(
       text_massage: params[:text_massage],
       sender: params[:sender],
@@ -97,14 +88,11 @@ class ApplicationController < Sinatra::Base
       typ: params[:type]
     )
     new_message.save
-
-
     msgs = get_messages(currentuser: params[:sender], activechat: params[:receiver])
     msgs.to_json
-    
   end
 
-  post "/businesses" do 
+  post '/businesses' do 
     new_business = Business.create(
       item_name: params[:item_name],
       phone: params[:phone],
@@ -115,45 +103,33 @@ class ApplicationController < Sinatra::Base
       description: params[:description]
     )
     new_business.save
-
   end
 
-  
-
-  get "/messages/?:currentuser/?:activechat" do
-    
+  get '/messages/?:currentuser/?:activechat' do
     msgs = self.get_messages(currentuser: params[:currentuser], activechat: params[:activechat])
     msgs.to_json
   end
 
-  # get "/contacts/?" do
-
-  # end
-
-  get "/balance/:phone" do
+  get '/balance/:phone' do
     bal = Account.find_by(phone: params[:phone])
     bal.to_json
   end
 
-  patch "/patch/receiver" do
+  patch '/patch/receiver' do
     send_to = Account.find_by(phone: params[:receiver])
     # amt = params[:text_massage]
     # puts amt
     send_amount = send_to.balance
-    send_to.balance = send_amount.to_i + (params[:text_massage].to_i)
+    send_to.balance = send_amount.to_i + params[:text_massage].to_i
     send_to.save
-    
-    
-    {message: "successfull"}.to_json
-  end
- 
-  patch "/patch/sender" do
-    send_from = Account.find_by(phone: params[:sender])
-    send_amount_to = send_from.balance
-    send_from.balance = send_amount_to.to_f - (params[:text_massage].to_f)
-    send_from.save
-    
     { message: 'successfull' }.to_json
   end
 
+  patch '/patch/sender' do
+    send_from = Account.find_by(phone: params[:sender])
+    send_amount_to = send_from.balance
+    send_from.balance = send_amount_to.to_f - params[:text_massage].to_f
+    send_from.save
+    { message: 'successfull' }.to_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,33 @@
-puts "🌱 Seeding spices..."
+puts '🌱 Seeding spices...'
 
 # Seed your database here
 User.all.delete_all
-User.create(username: "Emkay", phone: 742075647, password: "wueh", email: "emkay@gmail.com")
-User.create(username: "Sammy Doe", phone: 742075648, password: "wueh", email: "node@gmail.com")
-User.create(username: "Wendy", phone: 742, password: "wueh", email: "emkay@gmail.com" )
-User.create(username: "John", phone: 743, password: "wueh", email: "emkay@gmail.com")
+User.create(username: 'Emkay', phone: 742_075_647, password: 'wueh', email: 'emkay@gmail.com')
+User.create(username: 'Sammy Doe', phone: 742_075_648, password: 'wueh', email: 'node@gmail.com')
+User.create(username: 'Wendy', phone: 742, password: 'wueh', email: 'emkay@gmail.com')
+User.create(username: 'John', phone: 743, password: 'wueh', email: 'emkay@gmail.com')
+User.create(username: 'Chatty Bot', phone: 742, password: 'wueh', email: 'chattyboat@gmail.com')
 
 Message.all.delete_all
-Message.create(text_massage: "Hello Emkay", sender: 742, receiver: 742075647, typ: "text")
-Message.create(text_massage: "Hello Wendy How are you doing?", sender: 742075647, receiver: 742, typ: "text")
-Message.create(text_massage: "I am fine thank I really miss you,  how are you?", sender: 742, receiver: 742075647, typ: "text")
-Message.create(text_massage: "I am doing good I miss you too.", sender: 742075647, receiver: 742, typ: "text")
+Message.create(text_massage: 'Hello Emkay', sender: 742, receiver: 742_075_647, typ: 'text')
+Message.create(text_massage: 'Hello Wendy How are you doing?', sender: 742_075_647, receiver: 742, typ: 'text')
+Message.create(text_massage: 'I am fine thank I really miss you,  how are you?', sender: 742, receiver: 742_075_647, typ: 'text')
+Message.create(text_massage: 'I am doing good I miss you too.', sender: 742_075_647, receiver: 742, typ: 'text')
 Message.create(text_massage: 'Welcome to chatty you can ask me anything.', sender: 7426, receiver: 742, typ: 'text')
 
 Account.all.delete_all
-Account.create(phone: 742075647, balance: 35600)
-Account.create(phone: 742, balance: 15600)
-Account.create(phone: 743, balance: 10600)
-Account.create(phone: 742075648, balance: 2900)
+Account.create(phone: 742_075_647, balance: 35_600)
+Account.create(phone: 742, balance: 15_600)
+Account.create(phone: 743, balance: 10_600)
+Account.create(phone: 742_075_648, balance: 2900)
 
 Contact.all.delete_all
-Contact.create(sender: 742, receiver: 742075647, name: "Emkay")
-Contact.create(sender: 742, receiver: 743, name: "John")
-Contact.create(sender: 742, receiver: 742075648,  name:"Sam")
+Contact.create(sender: 742, receiver: 742_075_647, name: 'Emkay')
+Contact.create(sender: 742, receiver: 743, name: 'John')
+Contact.create(sender: 742, receiver: 742_075_648, name: 'Sam')
 
-#seeding bots
-Contact.create(sender: 7426, receiver: 742,  name:"Wendy")
-Contact.create(sender: 742, receiver: 7426,  name:"Chatty Bot")
+# seeding bots
+Contact.create(sender: 7426, receiver: 742,  name:'Wendy')
+Contact.create(sender: 742, receiver: 7426,  name:'Chatty Bot')
 
-puts "✅ Done seeding!"
+puts '✅ Done seeding!'
